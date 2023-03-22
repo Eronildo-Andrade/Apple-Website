01fdfae3db8fa8a47fdfad0e4ebe3b250d0a51bd
Eronildo-ht <erfilhoandrade@gmail.com>
2023-03-21 21:17:46 -0300

    criando uma tela de site da apple

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function changeColor(color) {
+    let circle = document.querySelector(".circle")
+    circle.style.background = color
+}
+
+function changeImage(image) {
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = image
+}
